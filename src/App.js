9d4a5e0842b3d7bb4f9f6f3b11f786f8f11a4dac
Ignacio Alonso <ignacio.alley@gmail.com>
2020-11-29 23:09:18 -0600

    Add the fart library

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fart = () => {
+    const options = [1, 2, 3, 4, 5, 6, 7, 8];
+    const randomOption = options[Math.floor(Math.random() * options.length)];
+
+    const audio = new Audio(
+      `https://www.soundjay.com/human/sounds/fart-0${randomOption}.mp3`
+    );
+    audio.play();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fart button</h1>
+      <div className="fart-btn" onClick={fart}>
+        Fart
+      </div>
     </div>
   );
 }
